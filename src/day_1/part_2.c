1c4e627cc6e4d2e090ddef1405eb1a3ace78b0c6
James King <james@agentultra.com>
2025-12-02 11:30:16 -0500

    Modify day_1/part_2.c
    Refactored some defines

--- src/day_1/part_2.c
+++ src/day_1/part_2.c
@@ -5,6 +5,7 @@
 #include "mod_arith.h"
 
 #define VAULT_MAX 99
+#define VAULT_MOD 100
 
 int main()
 {
@@ -19,22 +20,22 @@ int main()
 
         assert(current_num >= 0 && current_num <= VAULT_MAX);
 
-        int full_cycles = amt / 100;
-        int remaining_cycles = amt % 100;
+        int full_cycles = amt / VAULT_MOD;
+        int remaining_cycles = amt % VAULT_MOD;
 
         if (turn == 'L') {
             // calculate the remaining turn, if there is one
             int extra = current_num != 0 && remaining_cycles >= current_num ? 1 : 0;
 
             zeroes_count += full_cycles + extra;
-            current_num = mod_sub(current_num, amt, VAULT_MAX + 1);
+            current_num = mod_sub(current_num, amt, VAULT_MOD);
         }
 
         if (turn == 'R') {
-            int extra = current_num + remaining_cycles >= 100 ? 1 : 0;
+            int extra = current_num + remaining_cycles >= VAULT_MOD ? 1 : 0;
 
             zeroes_count += full_cycles + extra;
-            current_num = mod_add(current_num, amt, VAULT_MAX + 1);
+            current_num = mod_add(current_num, amt, VAULT_MOD);
         }
     }
 
