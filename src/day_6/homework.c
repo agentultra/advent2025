792a5ba0409017b44c8a6ef118f384ddc2c2b86d
James King <james@agentultra.com>
2025-12-08 12:08:12 -0500

    Modify src/day_6/homework.c
    Still no solution yet but getting closer?

--- src/day_6/homework.c
+++ src/day_6/homework.c
@@ -6,7 +6,7 @@
 
 #include "vec.h"
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 5096
 #define VEC_SIZE 1024
 
 struct mat_t {
@@ -14,25 +14,6 @@ struct mat_t {
     uint64_t h;
     struct vec_t* entries;
 };
-
-void rotate(struct mat_t* mat) {
-    struct vec_t* rotated = vec_copy(mat->entries);
-
-    for (int i = 0; i < mat->w * mat->h; ++i) {
-        int orig_row = i / mat->w;
-        int orig_col = i % mat->w;
-
-        int new_row = orig_col;
-        int new_col = mat->w - orig_row - 1;
-
-        vec_set(rotated, new_row * mat->w + new_col, vec_get(mat->entries, i));
-    }
-
-    struct vec_t* old_entries = mat->entries;
-    mat->entries = rotated;
-
-    vec_free(old_entries);
-}
 
 char* mat_get(struct mat_t* mat, uint64_t x, uint64_t y) {
     return vec_get(mat->entries, (mat->w * y) + x);
@@ -60,52 +41,55 @@ int main()
         size_t buf_len = strlen(buf);
         buf[buf_len-1] = '\0';
         buf_len--;
-        printf("%s\n", buf);
 
         char* token = strtok(buf, " ");
+
         if (mat.w == 0) w++;
-        printf("First: %s\n", token);
+
         vec_insert(mat.entries, token);
 
         while ((token = strtok(NULL, " "))) {
             if (mat.w == 0) w++;
+
             vec_insert(mat.entries, token);
         }
 
         if (mat.w == 0) mat.w = w;
+
         mat.h++;
     }
 
-    assert(mat.w == mat.h);
+    for (int i = 0; i < mat.w; ++i) {
+        printf("Doing column %d\n", i);
 
-    rotate(&mat);
+        char* op_sym = mat_get(&mat, i, mat.h-1);
+        uint64_t problem_total = 0;
 
-    int row = 0;
-
-    while (row < mat.h) {
-        char* op_sym = vec_get(mat.entries, row * mat.h);
-        uint64_t total = 0;
+        printf("Operator: %s\n", op_sym);
 
         if (strcmp(op_sym, "+") == 0) {
             op = &add;
+            problem_total = 0;
         } else if (strcmp(op_sym, "*") == 0) {
             op = &mul;
-            total = 1;
+            problem_total = 1;
         } else {
-            printf("Unknown operator: %s\n", op_sym);
+            printf("Unknown problem operator: %s\n", op_sym);
             exit(-1);
         }
 
-        for (int x = 1; x < mat.w; ++x) {
-            char* num_sym = mat_get(&mat, x, row);
-            uint64_t num = strtoll(num_sym, NULL, 10);
+        for (int j = 0; j < mat.h-1; ++j) {
+            char* num_sym = mat_get(&mat, i, j);
+            uint64_t num = strtoull(num_sym, NULL, 10);
 
-            total = op(total, num);
+            printf("Num: %" PRIu64 "\n", num);
+
+            problem_total = op(problem_total, num);
         }
 
-        grand_total += total;
+        printf("Total: %" PRIu64 "\n", problem_total);
 
-        row++;
+        grand_total += problem_total;
     }
 
     printf("Part 1: %" PRIu64 "\n", grand_total);
