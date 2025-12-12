6833bafcb4efeb2566faf3fdd6211354fcd1722d
James King <james@agentultra.com>
2025-12-12 10:28:21 -0500

    Modify src/day_9/theatre.c
    Add grid...

--- src/day_9/theatre.c
+++ src/day_9/theatre.c
@@ -28,32 +28,83 @@ uint64_t area(struct vec2_t* m, struct vec2_t* n) {
     return (a + 1) * (b + 1);
 }
 
+struct grid {
+    uint64_t w;
+    uint64_t h;
+    struct vec_t* entries;
+};
+
+struct grid* grid_new(uint64_t w, uint64_t h, void* default_elem, size_t elem_size) {
+    size_t entries_size = w * h;
+    struct vec_t* entries = vec_new(entries_size, elem_size);
+
+    for (size_t i = 0; i < entries_size; ++i) {
+        vec_insert(entries, default_elem);
+    }
+
+    struct grid* g = (struct grid*)malloc(sizeof (struct grid));
+
+    if (g == NULL) {
+        printf("Unable to allocation new grid: out of memory.\n");
+        exit(-1);
+    }
+
+    g->w = w;
+    g->h = h;
+    g->entries = entries;
+
+    return g;
+}
+
+void grid_set(struct grid* g, int x, int y, void* elem) {
+    int mod_x = x < 0 ? x + g->w : x % g->w;
+    int mod_y = y < 0 ? y + g->h : y % g->h;
+
+    int ix = (g->w * mod_y) + mod_x;
+    vec_set(g->entries, ix, elem);
+}
+
+enum tile {
+    EMPTY,
+    GREEN,
+    RED
+};
+
+static enum tile empty = EMPTY;
+static enum tile red = RED;
+
 int main()
 {
     char buf[BUF_SIZE];
     struct vec_t* points = vec_new(VEC_SIZE, sizeof (struct vec2_t));
+    uint64_t max_x = 0;
+    uint64_t max_y = 0;
 
     while (fgets(buf, sizeof (buf), stdin)) {
         struct vec2_t* v = (struct vec2_t*)malloc(sizeof (struct vec2_t));
 
         sscanf(buf, "%" PRIu64 ",%" PRIu64, &v->x, &v->y);
 
+        if (v->x > max_x) max_x = v->x;
+        if (v->y > max_y) max_y = v->y;
+
         vec_insert(points, v);
     }
 
     uint64_t max_area = 0;
+    struct grid* map = grid_new(max_x + 1, max_y + 1, &empty, sizeof (enum tile));
 
     for (int i = 0; i < vec_size(points); ++i) {
         struct vec2_t* v1 = vec_get(points, i);
+
+        grid_set(map, v1->x, v1->y, &red);
 
         for (int j = 0; j < vec_size(points); ++j) {
             if (i == j) continue;
 
             struct vec2_t* v2 = vec_get(points, j);
 
-//            printf("Testing: "); print_vec2(v1); printf(" "); print_vec2(v2); printf("\n");
             uint64_t a = area(v1, v2);
-//            printf("\tArea: %" PRIu64 "\n", a);
 
             if (a > max_area) {
                 max_area = a;
